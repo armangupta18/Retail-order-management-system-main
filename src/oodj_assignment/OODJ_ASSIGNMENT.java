fe7d028d47f2148ab87fc30d0b72fbb1aa7116b9
armangupta11 <armangupta1511@gmail.com>
2026-02-10 22:59:39 +0530

    Add files via upload

---
+++ src/oodj_assignment/OODJ_ASSIGNMENT.java
@@ -0,0 +1,12 @@
+
+package oodj_assignment;
+/*
+ * @author Ahmed Afnan Ali bin Ameer Ali (TP060379)
+ * @author Advait Sinha (TP054575)
+ */
+public class OODJ_ASSIGNMENT {
+    public static void main(String[] args) {
+        //To Start The Program From Login Window
+        new LoginJFrame().setVisible(true);
+    }
+}
